793dbb76bd8b14b5c9b838e1e1d40f653fcb19c4
0xfrian <0xfrian@gmail.com>
2023-09-10 16:51:53 -0700

    ✨ feat: checkhealth endpoint
    - created /src/lib.rs file
    - created /tests directory
    - created /tests/check_health.rs file

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use std::net::TcpListener;
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+async fn check_health() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+            App::new()
+                .route("/checkhealth", web::get().to(check_health))
+        })
+        .listen(listener)?
+        .run();
+
+    Ok(server)
+}
---
+++ tests/check_health.rs
@@ -0,0 +1,28 @@
+use rusty_krab::run;
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn check_health() {
+    let address = spawn_app();
+
+    let  client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("{}/checkhealth", address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
